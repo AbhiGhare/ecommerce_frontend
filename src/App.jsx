42ef58cc77e1f2e7b3c8e8e09e9f19c1a2d6ff18
AbhiGhare <ghareabhishek4@gmail.com>
2024-08-31 00:34:19 +0530

    update theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,39 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
-import ProductDetails from './pages/ProductDetails';
-import Cart from './pages/Cart';
-import MainHeader from './components/MainHeader';
-import SecondaryHeader from './components/SecondaryHeader';
-import Footer from './components/Footer';
-import FavoritesPage from './components/FavoritesPage';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import ProductUpload from './pages/ProductUpload';
+// import HomePage from './HomePage';
+// import AboutPage from './AboutPage';
+// import ContactPage from './ContactPage';
+import HomePage from './components/HomePage';
+import Productdetails from './components/productdetails';
+import Grid from './components/grid';
+import Cart from './components/Cart';
+import Header from './components/Header';
+import Login from './components/Login';
+import Favorites from './components/Favorites';
+import SignUpPage from './components/SignUpPage';
+import PaymentSuccess from './components/PaymentSuccess';
+import PaymentCancelled from './components/PaymentCancelled';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; // Import CSS for toast notifications
-import PaymentSuccess from './pages/PaymentSuccess';
-import PaymentCancelled from './pages/PaymentCancelled';
-import { useDispatch } from 'react-redux';
-import { fetchCartData } from './features/cart/cartSlice';
-import CategoryPage from './pages/CategoryPage';
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const token = localStorage.getItem('nayahe_hai');
-    if (token) {
-      dispatch(fetchCartData());
-    }
-  }, [dispatch]);
+function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        {/* Main Header with Search Box */}
-        <MainHeader />
-        
-        {/* Secondary Header with Navigation Links */}
-        <SecondaryHeader />
-
-        {/* Main Content */}
-        <main className="flex-grow mt-32">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<ProductList />} />
-            <Route path="/favorites" element={<FavoritesPage />} />
-            <Route path="/products/:id" element={<ProductDetails />} />
-            <Route path="/categoryName/:categoryName" element={<CategoryPage />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/uploadProduct" element={<ProductUpload />} />
-            <Route path="/sucess" element={<PaymentSuccess />} />
-            <Route path="/cancel" element={<PaymentCancelled />} />
-          </Routes>
-        </main>
-        
-        {/* Footer */}
-        <Footer />
-        <ToastContainer
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product-details/:id" element={<Productdetails />} />
+        <Route path="/categoryName/:categoryName" element={<Grid />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/sucess" element={<PaymentSuccess />} />
+        <Route path="/cancel" element={<PaymentCancelled />} />
+        {/* <Route path="/contact" element={<ContactPage />} /> */}
+        {/* Add more routes as needed */}
+      </Routes>
+      <ToastContainer
           position="top-right"
           autoClose={5000}
           hideProgressBar={false}
@@ -68,9 +45,8 @@ const App = () => {
           pauseOnHover
           theme="colored"
         />
-      </div>
     </Router>
   );
-};
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { store } from './app/store';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import your CSS files
+// import './assets/css/bootstrap.min.css';
+import './assets/css/LineIcons.3.0.css';
+import './assets/css/tiny-slider.css';
+import './assets/css/glightbox.min.css';
+import './assets/css/main.css';
+// Import Bootstrap JavaScript
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Provider } from 'react-redux';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { store } from './redux/store/store.js';
+createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <App />
-</Provider>,
-);
+  <StrictMode>
+    <App />
+  </StrictMode>
+  </Provider>,
+)
